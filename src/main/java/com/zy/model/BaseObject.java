28d625c95924d903cefe0d4aaa14e730ec63c928
zhongyuan <zhongyuan966@gmail.com>
2019-06-29 14:42:53 +0800

    second commit

---
+++ src/main/java/com/zy/model/BaseObject.java
@@ -0,0 +1,14 @@
+package com.zy.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author zy
+ * @date 2019/6/29 14:41
+ */
+@Data
+public class BaseObject implements Serializable {
+
+}
